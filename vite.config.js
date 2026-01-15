2657f487cbc1fef52ea5713a0aa597725d0859bc
akang05 <alvinkang758@gmail.com>
2026-01-14 21:58:33 -0500

    hi

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Profile-App/',
   plugins: [react()],
+  base: '/Profile-App/',
 })
